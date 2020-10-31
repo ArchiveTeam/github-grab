9aeb5012a736fc011253a93056dc3f85f5b20841
arkiver <arkiver@protonmail.com>
2020-10-31 01:19:34 +0100

    Version 20201031.02. Support Wget-AT version .20.3-at.20201030.01.

--- pipeline.py
+++ pipeline.py
@@ -43,7 +43,8 @@ WGET_AT = find_executable(
     [
         'GNU Wget 1.20.3-at.20200902.01',
         'GNU Wget 1.20.3-at.20200917.01',
-        'GNU Wget 1.20.3-at.20200919.01'
+        'GNU Wget 1.20.3-at.20200919.01',
+        'GNU Wget 1.20.3-at.20201030.01'
     ],
     ['./wget-at']
 )
@@ -56,7 +57,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201031.01'
+VERSION = '20201031.02'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'github'
 TRACKER_HOST = 'trackerproxy.archiveteam.org'
